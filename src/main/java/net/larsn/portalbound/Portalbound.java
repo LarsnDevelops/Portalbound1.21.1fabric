2cd7799a392d0180ea1d478859e35bf15bc9ef74
LarsnDevelops <larsonbjackson@gmail.com>
2025-05-02 12:56:56 -0600

    Initial commit

---
+++ src/main/java/net/larsn/portalbound/Portalbound.java
@@ -0,0 +1,16 @@
+package net.larsn.portalbound;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Portalbound implements ModInitializer {
+	public static final String MOD_ID = "portalbound";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/larsn/portalbound/PortalboundClient.java
@@ -0,0 +1,10 @@
+package net.larsn.portalbound;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PortalboundClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
